835a838b99da23ed7a33145fb516720bf1c4660f
Shivam Negi <dev.shivamnegi@gmail.com>
2025-06-30 02:02:09 +0530

    resolving CORS issue

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174'] : '*',
+    origin: ['https://khidki-frontend.vercel.app', 'http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
